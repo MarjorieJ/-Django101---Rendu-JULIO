e0bd2cfffc3b62a4471669ce6d8b06df3a5fc46b
MarjorieJ <marjorie.julio@depinfonancy.net>
2016-10-29 17:14:08 +0100

    Correction1

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -18,7 +18,7 @@ class IndexView(generic.ListView):
 		
 class DetailView(generic.DetailView):
 	model=Question
-	template_name='polls/detail.html'
+	template_name='polls\detail.html'
 	def get_queryset(self):
 		#excludes any questions that aren't published yet
 		return Question.objects.filter(pub_date__lte=timezone.now())
